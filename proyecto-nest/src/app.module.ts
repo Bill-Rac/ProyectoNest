ba2977b3768839187bdeb7a2d7e0d48e51a764f3
Bill-Rac <askykerx@gmail.com>
2023-10-14 21:25:42 -0500

    Creacion de los modulos en el proyecto

--- proyecto-nest/src/app.module.ts
+++ proyecto-nest/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { PaymentsModule } from './payments/payments.module';
+import { OrdersModule } from './orders/orders.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { CustomersModule } from './customers/customers.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, UsersModule, PaymentsModule, OrdersModule, InventoryModule, CustomersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
